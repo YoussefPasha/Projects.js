319103c7ed32ff13e83f27296befc6d3e62975b1
YoussefPasha <jojoyossefbasha010@gmail.com>
2021-01-29 23:56:15 +0200

    done with render cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import { Card } from "./container";
 import { ExpandingCards } from "./components";
 
 export default class componentName extends Component {
   render() {
     return (
-      <div>
-        <ExpandingCards />
+      <div
+        style={{
+          display: "flex",
+          flexWrap: "wrap",
+          justifyContent: "center",
+        }}
+      >
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
       </div>
     );
   }
