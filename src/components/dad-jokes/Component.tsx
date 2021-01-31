384b76f5379aee36ec36960ae77508561173e192
YoussefPasha <jojoyossefbasha010@gmail.com>
2021-01-31 02:57:34 +0200

    done with class

--- src/components/dad-jokes/Component.tsx
+++ src/components/dad-jokes/Component.tsx
@@ -1,14 +1,32 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import MainComponent from "./components";
 
 const Component = () => {
+  const [joke, setJoke] = useState("");
+
+  const fetchJoke = async () => {
+    setJoke("loading...");
+    const config = {
+      headers: {
+        Accept: "application/json",
+      },
+    };
+    const res = await fetch("https://icanhazdadjoke.com", config);
+    const data = await res.json();
+    setJoke(data.joke);
+  };
+  if (joke === "") {
+    fetchJoke();
+  }
   return (
     <Fragment>
       <MainComponent>
         <MainComponent.Container>
           <MainComponent.Txt>Don't Laugh Challenge</MainComponent.Txt>
-          <MainComponent.Joke>Joke</MainComponent.Joke>
-          <MainComponent.Btn>Get another Joke</MainComponent.Btn>
+          <MainComponent.Joke>{joke}</MainComponent.Joke>
+          <MainComponent.Btn onClick={fetchJoke}>
+            Get another Joke
+          </MainComponent.Btn>
         </MainComponent.Container>
       </MainComponent>
     </Fragment>
